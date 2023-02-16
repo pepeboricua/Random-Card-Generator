be4dc51fa80d51f90dd991d9e7ee2cffb4cb5f0e
pepeboricua <joseoronozjr@gmail.com>
2023-02-16 01:57:11 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suits = [
+    "<span class='red'>&hearts;</span>",
+    "<span class='black'>&spades;</span>",
+    "<span class='black'>&clubs;</span>",
+    "<span class='red'>&diam;</span>"
+  ];
+
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+  document.querySelector(".top-suit").innerHTML = suit;
+  document.querySelector(".bottom-suit").innerHTML = suit;
+  document.querySelector(".number").innerHTML =
+    values[Math.floor(Math.random() * values.length)];
 };
